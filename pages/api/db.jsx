f2231b077bc38a3eb84c315b5b8a0f2862a82bff
RyujuAono <ryu050102@gmail.com>
2021-01-02 22:17:48 +0900

    fixed: change api res

--- pages/api/db.jsx
+++ pages/api/db.jsx
@@ -3,30 +3,37 @@ import UpdateDoc from '../../components/Services/UpdateDoc'
 import DeleteDoc from '../../components/Services/DeleteDoc'
 
 export default (req, res) => {
+
+    let statusCode = 404
+    let json = { 'message': "not found"}
+
     switch (req.method) {
         case 'POST': {
-            res.statusCode = 200
+            statusCode = 200
             console.log(req.body)
             if(req.body.id === null){
                 new Promise((resolve, reject) => { AddDoc(resolve, reject, req.body.collection, req.body.params) })
-                res.json({ message: 'Add Success' })
+                json = { 'message': 'Add Success' }
             }
             else
             {
                 new Promise((resolve, reject) => { UpdateDoc(resolve, reject, req.body.collection, req.body.id, req.body.params) })
-                res.json({ message: 'Update Success' })
+                json = { 'message': 'Update Success' }
             }
             break;
         }
         case 'DELETE': {
             new Promise((resolve, reject) => { DeleteDoc(resolve, reject, req.query.collection, req.query.id) })
-            res.statusCode = 200
-            res.json({ message: 'Delete Success' })
+            statusCode = 200
+            json = { 'message': 'Delete Success' }
             break;
         }
         default: {
-            res.statusCode = 405
-            res.json({ message: 'Your Method Not Allowed' })
+            statusCode = 405
+            json = { 'message': 'Your Method Not Allowed' }
         }
     }
+
+    res.statusCode = statusCode
+    res.json(json)
 }
