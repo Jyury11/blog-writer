ebacada917850b853aa82ca6ccfbc95a0633e4a0
RyujuAono <ryu050102@gmail.com>
2021-01-04 15:55:42 +0900

    ref: delete comment and file

--- components/Services/CheckWriters.jsx
+++ components/Services/CheckWriters.jsx
@@ -54,45 +54,4 @@ export default function CheckWriters(resolve, reject, getCollection, updateColle
     }).catch(error => {
         reject([])
     })
-
-    // const writersInfo = writers.filter((doc) => doc.id === 'info')
-
-    // const writerDate = new Date(writersInfo[0].date.seconds * 1000)
-    // const now = new Date()
-
-    // if(writerDate < now){
-
-    //     const nextDate = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
-
-    //     const target = writers.sort((a,b) =>{
-    //         if (a.priority > b.priority) {
-    //             return -1;
-    //         }
-    //         else
-    //         {
-    //             return 1;
-    //         }
-    //     })
-
-    //     let batch = db.batch()
-
-    //     db.collection('writer')
-    //     .get()
-    //     .then(snapshot => {
-    //         snapshot.docs.map(doc => {
-    //             if(doc.id !== 'info'){
-    //                 batch.delete(doc.ref)
-    //             }
-    //             else
-    //             {
-    //                 batch.update(doc.ref, { date: nextDate})
-    //             }
-    //         })
-
-    //         batch.commit()
-    //     })
-
-
-    //     UpdateBatch('posts', target[0].id)
-    // }
 }
